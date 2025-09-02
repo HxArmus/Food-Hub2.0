575bc988bca3a19de019936354202b880c1e3a2e
HxArmus <kaustubhjawde@gmail.com>
2025-09-02 14:37:58 +0530

    header completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/pages/Home/Home'
+import Cart from './components/pages/Cart/Cart'
+import Placeorder from './components/pages/Placeorder/Placeorder'
+
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/Cart' element={<Cart />} />
+      <Route path='/Order' element={<Placeorder />} />
+    </Routes>
     </div>
   )
 }
